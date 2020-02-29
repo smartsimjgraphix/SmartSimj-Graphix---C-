4842708b771bca789c80468e7bdedcc945e9fa11
smartsimjgraphix <42468344+smartsimjgraphix@users.noreply.github.com>
2020-02-29 20:45:50 +0000

    Lesson One in C++ Programming

---
+++ ConsoleAppC++/ConsoleAppC++/Apples.cpp
@@ -0,0 +1,20 @@
+#include "Apples.h"
+#include <iostream>
+using namespace std;
+
+Apples::Apples()
+{
+	int main()
+	{
+		int a;
+	cout << "PLease Enter a number" << endl;
+	cin >> a;
+	cout << "Your input was" << a << endl;
+	}
+}
+
+
+Apples::~Apples()
+{
+
+}
---
+++ ConsoleAppC++/ConsoleAppC++/Apples.h
@@ -0,0 +1,8 @@
+#pragma once
+class Apples
+{
+public:
+	Apples();
+	~Apples();
+};
+
